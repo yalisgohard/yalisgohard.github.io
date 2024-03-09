8e8c0fc5982d8ce3759c1c8d1de138c87b96184e
Yalis-supinfo <yalis.gohard@supinfo.com>
2024-03-09 18:16:52 +0100

    invitation

---
+++ invitation/script.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".ellipse").forEach((layer) => {
+  const speed = layer.dataset.speed;
+  gsap.from(layer, {
+    scrollTrigger: {
+      trigger: layer,
+      scroller: "main",
+      start: "top bottom",
+      end: "bottom top",
+      scrub: true,
+    },
+    y: speed * 100,
+    duration: 1,
+    ease: "none",
+  });
+});
+
+gsap.utils.toArray(".card img").forEach((card, index) => {
+  gsap.fromTo(
+    card,
+    {
+      scale: 1.5,
+    },
+    {
+      scrollTrigger: {
+        trigger: card,
+        scroller: "main",
+        start: "top center",
+        toggleActions: "restart none none none",
+      },
+      scale: 1,
+      duration: 0.3,
+      delay: index * 0.3,
+    }
+  );
+});
+
+document
+  .querySelector("#rsvp form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission
+    // demande si la personne est sûr de vouloir envoyer le formulaire
+    if (this.querySelector("#check").checked) {
+      alert("Génial on se voit bientôt!");
+    } else {
+      alert("Dommage, à une prochaine fois!");
+    }
+    this.submit();
+  });
